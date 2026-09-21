4a585f451742a62ba3add178b295307693a6c496
Vishal Rabadiya <rabadiya.vishal@thinksys.com>
2026-09-21 22:39:04 +0530

    libraries, svg changes and other changes

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,12 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(defaultConfig, {
+  transformer: { babelTransformerPath: require.resolve('react-native-svg-transformer') },
+  resolver: {
+    assetExts: assetExts.filter(e => e !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+});
